4f10ff226645eb6b7ef8a79a371cacd345c56d98
Anastasia <anastasia.prikhodina@msg.group>
2021-05-31 22:00:50 +0200

    separate folder for each tag

--- dataset_generation/fragments_extraction.py
+++ dataset_generation/fragments_extraction.py
@@ -1,35 +1,62 @@
 import json
 from PIL import Image
-import skimage.io as io
 
-JSON_DATA_PATH = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/0ec33ccc8c6a72fefa3d0cdf271757ee-asset.json'
-JPG_INPUT_PATH = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/StAF%20F%20196-1_104_0033.jpg'
-IM_OUT_PATH = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped/'
+JSON_DATA_PATH = "C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT//output_photo_StAFF F 196-10_104/wgm_subset_export.json"
+JPG_ORIGIM_PATH = "C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/vott-json-export/"
 
-with open(JSON_DATA_PATH) as json_file:
-    data = json.load(json_file)
-    data = data['regions']
+IM_OUT_PATH_STAMP = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped_stamp/'
+IM_OUT_PATH_SIGNATURE = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped_signature/'
+IM_OUT_PATH_HANDWRITTEN = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped_handwritten/'
+IM_OUT_PATH_MACHINEPRINTED = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped_machineprinted/'
+IM_OUT_PATH_NOISE = 'C:/Users/prikha/Downloads/BA/Datasets/wgm-subset/output VoTT/output_photo_StAFF F 196-10_104/cropped_noise/'
 
-#print(json.dumps(data, indent=2))
 
-print(len(data))
+def json2fragments(data):
 
-for asset in range(0, 1):  #len(data)
-    id = data[asset]['id']
-    print('id: ', id)
-    #path = data[asset]['path']
-    #print('path: ', path)
-    tags = data[asset]['tags']
-    print('tags: ', tags)
-    points = data[asset]['points']
-    print('points: ', points)
-    print(len(points))
+    #print(json.dumps(data, indent=2))
+    #print(len(data.keys()))
+    #print(len(data))
 
-    for point in points:
-        print(point)
+    keys_arr = list(data.keys())
+    for key in range(len(keys_arr)):
+        name_im = data[keys_arr[key]]['asset']['name']
+        asset = data[keys_arr[key]]['regions'][0]
+        print(asset)
 
-        img = Image.open(JPG_INPUT_PATH)
-        img2 = img.crop((0, 0, 150, 150))
-        img2.save(IM_OUT_PATH + id + '_cropped.jpg', 'JPEG')
+        id = asset['id']
+        print('id: ', id)
+        #path = asset['path']
+        #print('path: ', path)
+        tags = asset['tags'][0]
+        print('tags: ', tags)
+        points = asset['points']
+        print('points: ', points)
+        print(len(points))
+    #
+        x_left, x_right = points[0]['x'], points[1]['x']
+        y_top, y_bottom = points[0]['y'], points[2]['y']
 
+        img = Image.open(JPG_ORIGIM_PATH + name_im)
+        img2 = img.crop((x_left, y_top, x_right, y_bottom))
+
+        if tags == 'stamp':
+           img2.save(IM_OUT_PATH_STAMP + id + '_cropped.jpg', 'JPEG')
+        elif tags == 'signature':
+            img2.save(IM_OUT_PATH_SIGNATURE + id + '_cropped.jpg', 'JPEG')
+        elif tags == 'handwritten':
+            img2.save(IM_OUT_PATH_HANDWRITTEN + id + '_cropped.jpg', 'JPEG')
+        elif tags == 'machineprinted':
+            img2.save(IM_OUT_PATH_MACHINEPRINTED + id + '_cropped.jpg', 'JPEG')
+        elif tags == 'noise':
+            img2.save(IM_OUT_PATH_NOISE + id + '_cropped.jpg', 'JPEG')
+
+        print("------------------END OF A LOOP----------------")
+
+
+if __name__ == '__main__':
+
+    with open(JSON_DATA_PATH) as json_file:
+        data = json.load(json_file)
+        data = data['assets']
+        json2fragments(data)
 
